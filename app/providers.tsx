e1af7c221ed9de3b5353d7c00f2dde540f4099b5
Claire Gormley <cgormley@engagez.com>
2026-02-12 23:57:13 -0700

    updates

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <GoogleOAuthProvider clientId="YOUR_COPIED_CLIENT_ID.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="643516163366-2p8gonpdt21vmpquegh117raodgcpfci.apps.googleusercontent.com">
       {children}
     </GoogleOAuthProvider>
   );
